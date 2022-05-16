6905dc4397b1a4543c93f6513811dfd0711acfbf
thaisascastro <thaisascastro@gmail.com>
2022-05-16 20:53:19 -0300

    feat: create program4

---
+++ Programa4/src/Class.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+class Programa4 {
+    public static void main(String[] args) {
+        int n1, n2, div;
+        double pot;
+        String msg = "";
+
+        n1 = Integer.parseInt(JOptionPane.showInputDialog("Digite um número inteiro: "));
+        n2 = Integer.parseInt(JOptionPane.showInputDialog("Digite outro número inteiro: "));
+
+        div = (int)n1 / (int)n2;
+        pot = Math.pow(n1, n2);
+
+        msg = msg + "quociente da divisão de " + n1 + " por " + n2 + " = " + div + "\n";
+        msg = msg + "potência de " + n1 + " por " + n2 + " = " + pot + "\n";
+        JOptionPane.showMessageDialog(null, msg);
+
+        System.exit(0);
+    }
+}
