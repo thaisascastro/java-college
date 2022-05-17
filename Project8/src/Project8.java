d8a5c1df8131f7260c6fe0b1c20e9098f66afccd
thaisascastro <thaisascastro@gmail.com>
2022-05-17 17:00:24 -0300

    feat: create modularization

--- Project8/src/Project8.java
+++ Project8/src/Project8.java
@@ -1,6 +1,49 @@
- class Project8 {
+import javax.swing.*;
+
+class Project8 {
      public static void soma() {
          int n1, n2;
-         n1 = Integer.parseInt()
+         n1 = Integer.parseInt(JOptionPane.showInputDialog("Digite um número:"));
+         n2 = Integer.parseInt(JOptionPane.showInputDialog("Digite outro número:"));
+
+         JOptionPane.showMessageDialog(null, "A soma é " + (n1 + n2));
      }
+
+    public static void subtracao(int x, int y) {
+        int s;
+        s = x - y;
+
+        JOptionPane.showMessageDialog(null, "A diferença é: " + s);
+    }
+
+    public static int produto() {
+        int n1, n2;
+        n1 = Integer.parseInt(JOptionPane.showInputDialog("Digite um número:"));
+        n2 = Integer.parseInt(JOptionPane.showInputDialog("Digite outro número:"));
+
+        return (n1 * n2);
+    }
+
+    public static double divisao(int x, int y) {
+        double d;
+        d = (double)x / (double) y;
+
+        return d;
+    }
+
+    public static void main(String[] args) {
+        int n1, n2, s;
+        double r;
+
+        soma();
+        n1 = Integer.parseInt(JOptionPane.showInputDialog("Digite um número:"));
+        n2 = Integer.parseInt(JOptionPane.showInputDialog("Digite outro número:"));
+        subtracao(n1, n2);
+        s = produto();
+        JOptionPane.showMessageDialog(null, "O produto é " + s);
+        r = divisao(n1, n2);
+        JOptionPane.showMessageDialog(null, "A divisão é: " + r);
+        System.exit(0);
+
+    }
 }
