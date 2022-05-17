15d2ed480394f09d135a928ad6ea15319c920719
thaisascastro <thaisascastro@gmail.com>
2022-05-17 17:47:09 -0300

    feat: add product

---
+++ Product10/src/Product10.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+
+class Product10 {
+
+    public static void soma (int vet[]) {
+        int s = 0;
+        for (int i= 0; i< vet.length; i++) {
+            s = s + vet[i];
+        }
+        JOptionPane.showMessageDialog(null, "A somatória é " + s);
+    }
+
+    public static int produto (int vet[]) {
+        int p = 1;
+        for (int i= 0; i< vet.length; i++) {
+            p = p * vet[i];
+        }
+        return p;
+    }
+
+     public static void main(String[] args) {
+         int s=0, vetor[] = {2, 4, 6, 8, 10};
+         int r;
+
+         soma(vetor);
+         r = produto(vetor);
+         JOptionPane.showMessageDialog(null, "A produtoria é " + r);
+         System.exit(0);
+     }
+}
+
